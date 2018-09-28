0d0a692dfeb6208373ff5494e4ef2e5ec5486830
William Heyman Krill <whk@magenta.dk>
2018-09-28 16:18:03 +0200

    Add views and urls, and fix import of app in settings.py

--- src/aka/settings.py
+++ src/aka/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'akasite.apps.AkasiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'akasite',
 ]
 
 MIDDLEWARE = [
--- src/akasite/urls.py
+++ src/akasite/urls.py
@@ -1,4 +1,10 @@
+from django.conf.urls import url
+
+from akasite import views
+
 app_name = 'akasite'
 
 urlpatterns = [
+    url(r'^index$', views.IndexView.as_view(), name='index'),
+    url(r'^indberetning$', views.Indberetning.as_view(), name='indberetning'),
 ]
