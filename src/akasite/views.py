ff4af64c42955b8b9510491951ff4a153e79ee0c
William Heyman Krill <whk@magenta.dk>
2018-10-05 10:50:14 +0200

    Merge clutter from development

--- src/akasite/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class IndexView(TemplateView):
-    template_name = 'akasite/index.html'
-
-
-class Indberetning(TemplateView):
-    template_name = 'akasite/indberet_fordring.html'
