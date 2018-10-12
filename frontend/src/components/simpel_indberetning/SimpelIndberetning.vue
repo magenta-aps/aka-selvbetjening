84b3a5866e0c0d7597989454a0ae73ddb2d55997
William Heyman Krill <whk@magenta.dk>
2018-10-12 12:02:23 +0200

    Add simple form component to test rest interface

---
+++ frontend/src/components/simpel_indberetning/SimpelIndberetning.vue
@@ -0,0 +1,78 @@
+<template>
+
+    <article class="test">
+
+        <h1>Simpel indberetning</h1>
+
+        <form @submit.prevent="sendFormRequest()">
+
+            <fieldset>
+                <label for="fordringshaver-input">Fordringshaver</label>
+                <input id="fordringshaver-input" type="text" value="NOGH2342" v-model="fordringshaver">
+            </fieldset>
+
+            <fieldset>
+                <label for="debitor-input">Debitor</label>
+                <input id="debitor-input" type="text" v-model="debitor">
+            </fieldset>
+
+            <fieldset>
+                <label for="anden-fordringshaver-input">Anden fordringshaver</label>
+                <input id="anden-fordringshaver-input" type="text" v-model="fordringshaver2">
+            </fieldset>
+
+            <fieldset>
+                <input type="submit" value="Send">
+            </fieldset>
+
+        </form>
+
+    </article>
+
+</template>
+
+<script>
+
+    import axios from 'axios'
+
+    export default {
+        data: function() {
+            return {
+                fordringshaver: null,
+                fordringshaver2: null,
+                debitor: null,
+            }
+        },
+        methods: {
+            sendFormRequest: function() {
+
+                let formdata = new FormData()
+                formdata.append('fordringshaver', this.fordringshaver)
+                formdata.append('debitor', this.debitor)
+                formdata.append('fordringshaver2', this.fordringshaver2)
+
+                axios({
+                    url: '/filupload',
+                    data: formdata,
+                    method: 'post',
+                    headers: {
+                        'X-CSRFToken': this.csrftoken,
+                        'X-AKA-BRUGER': 'Unknown'
+                    }
+                })
+                .then(res => {
+                    console.log('Server response!')
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.log('there was an error')
+                    console.log(err.message)
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import App from './App.vue'
 import TableOfContents from './components/table_of_contents/TableOfContents.vue'
 import FormExample1 from './components/form_example_1/FormExample1.vue'
 import FormExample2 from './components/form_example_2/FormExample2.vue'
+import IndberetFordring from './components/indberet_fordring/IndberetFordring.vue'
+import SimpelIndberetning from './components/simpel_indberetning/SimpelIndberetning.vue'
 
 //
 // Vue.use(VueI18n)
@@ -16,7 +18,9 @@ Vue.use(VueI18n)
 const routes = [
   { path: '/', component: TableOfContents },
   { path: '/form1', component: FormExample1 },
-  { path: '/form2', component: FormExample2 }
+  { path: '/form2', component: FormExample2 },
+  {path: '/indberetning', component: IndberetFordring},
+  {path: '/simpel', component: SimpelIndberetning},
 ]
 
 const router = new VueRouter({
