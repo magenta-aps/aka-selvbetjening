13427dbf0be3f9ef08ad1ff2b80c5ce3af48d804
mneidhardtmagenta <min@magenta.dk>
2018-10-15 15:01:38 +0200

    Improved tests for validation.

--- backend/akasite/rest/tests.py
+++ backend/akasite/rest/tests.py
@@ -7,54 +7,32 @@ import json
 # Create your tests here.
 class BasicTestCase(TestCase):
     def setUp(self):
-        pass
+        self.schema = {
+            "type" : "object",
+            "properties" : {
+                "price" : {"type" : "number"},
+                "name" : {"type" : "string"},
+                "year" : {"type" : "number"},
+            },
+            "required": ["price", "name", "year"],
+        }
+        self.validator = JsonValidator(self.schema)
+        self.assertTrue(type(self.validator) is JsonValidator)
 
     # Test module JsonValidator.
     # -------------------------- 
     def test_Validator_1(self):
-        schema = {
-            "type" : "object",
-            "properties" : {
-                "price" : {"type" : "number"},
-                "name" : {"type" : "string"},
-                "year" : {"type" : "number"},
-            },
-            "required": ["price", "name", "year"],
-        }
-        valid = JsonValidator(schema)
-        self.assertTrue(type(valid) is JsonValidator)
-
         jsonOK =   {"name" : "Eggs", "price" : 34.99, "year": 2018}
-        self.assertTrue(valid.valid(jsonOK))
+        self.assertTrue(self.validator.valid(jsonOK))
+        lasterror = self.validator.getLasterror()
+        self.assertTrue(lasterror is None)
 
     def test_Validator_2(self):
-        schema = {
-            "type" : "object",
-            "properties" : {
-                "price" : {"type" : "number"},
-                "name" : {"type" : "string"},
-                "year" : {"type" : "number"},
-            },
-            "required": ["price", "name", "year"],
-        }
-        valid = JsonValidator(schema)
-        self.assertTrue(type(valid) is JsonValidator)
-
         jsonFAIL = {"name" : "Eggs", "price" : 217.00}
-        self.assertFalse(valid.valid(jsonFAIL))
+        self.assertFalse(self.validator.valid(jsonFAIL))
+        errmsg = self.validator.getLasterror().message
+        self.assertTrue(type(errmsg) is str)
 
     def test_Validator_3(self):
-        schema = {
-            "type" : "object",
-            "properties" : {
-                "price" : {"type" : "number"},
-                "name" : {"type" : "string"},
-                "year" : {"type" : "number"},
-            },
-            "required": ["price", "name", "year"],
-        }
-        valid = JsonValidator(schema)
-        self.assertTrue(type(valid) is JsonValidator)
-
         jsonFAIL = {"name" : "Eggs", "price" : "NaN"}
-        self.assertFalse(valid.valid(jsonFAIL))
+        self.assertFalse(self.validator.valid(jsonFAIL))
--- backend/akasite/rest/validation.py
+++ backend/akasite/rest/validation.py
@@ -10,6 +10,7 @@ class JsonValidator(Validator):
     '''
     def __init__(self, schema):
         self.schema = schema
+        self.lasterror = None
 
     def setSchema(self, schema):
         self.schema = schema
@@ -17,9 +18,14 @@ class JsonValidator(Validator):
     def getSchema(self, schema):
         return self.schema
 
+    def getLasterror(self):
+        return self.lasterror
+
     def valid(self, object):
         try:
             jsonschema.validate(object, self.schema)
+            self.lasterror = None
             return True
         except jsonschema.exceptions.ValidationError as err:
+            self.lasterror = err
             return False
