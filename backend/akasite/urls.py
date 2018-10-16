d40db90399c33ff074ef157718bc5b3cb14589ac
Jakob <jakob@magenta.dk>
2018-10-16 13:07:17 +0200

    removed unused template

--- backend/akasite/urls.py
+++ backend/akasite/urls.py
@@ -7,7 +7,6 @@ app_name = 'akasite'
 
 urlpatterns = [
     url(r'^index$', htmlviews.IndexView.as_view(), name='index'),
-    url(r'^indberetning$', htmlviews.Indberetning.as_view(), name='indberetning'),
     url(r'^inkassosag$', inkassosag.InkassoSag.as_view(), name='inkassosag'),
     url(r'^debitor$', debitor.Debitor.as_view(), name='debitor'),
     url(r'^filupload$', fileupload.FileUpload.as_view(), name='filupload'),
