e500db5d4a69b31765ee1f0c8674f6390ed49748
Frank <frank@MacBook-Pro-3.local>
2018-10-19 12:36:22 +0200

    Improve webpack build to cater for IE11, add example of converting CSV to JSON in browser

--- frontend/src/components/table_of_contents/TableOfContents.vue
+++ frontend/src/components/table_of_contents/TableOfContents.vue
@@ -9,6 +9,10 @@
             <img class="tile-icon" src="/static/img/SVG/sullisivik_skatter-afgifter-og-gaeld.svg" alt="">
             <span>Form example 2</span>
         </router-link>
+        <router-link class="toc-link tile" to="/upload">
+            <img class="tile-icon" src="/static/img/SVG/sullisivik_skatter-afgifter-og-gaeld.svg" alt="">
+            <span>Upload files with JSON example</span>
+        </router-link>
     </nav>
 
 </template>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,11 @@ import VueRouter from 'vue-router'
 import VueI18n from 'vue-i18n'
 import { messages } from './i18n/Messages.js'
 import App from './App.vue'
+
 const TableOfContents = () => import('./components/table_of_contents/TableOfContents.vue')
 const FormExample1 = () => import('./components/form_example_1/FormExample1.vue')
 const FormExample2 = () => import('./components/form_example_2/FormExample2.vue')
-
-//
-// Vue.use(VueI18n)
+const Experiment = () => import('./components/experiment_with_file_upload/Example.vue')
 
 Vue.use(VueRouter)
 Vue.use(VueI18n)
@@ -16,7 +15,8 @@ Vue.use(VueI18n)
 const routes = [
   { path: '/', component: TableOfContents },
   { path: '/form1', component: FormExample1 },
-  { path: '/form2', component: FormExample2 }
+  { path: '/form2', component: FormExample2 },
+  { path: '/upload', component: Experiment }
 ]
 
 const router = new VueRouter({
