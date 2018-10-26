d538c3b897c1bd375657092b15f752b2c1402d80
iamfrank <frank@magenta.dk>
2018-10-26 10:04:13 +0200

    additions to designguide

--- frontend/src/components/designguide/DesignGuide.vue
+++ frontend/src/components/designguide/DesignGuide.vue
@@ -4,29 +4,100 @@
 
         <h1>Designguide</h1>
 
-        <p><a href="http://sullissivik.designguiden.dk/website">Designguiden kan findes her.</a></p>
+        <p>Designguiden fra 1508 <a href="http://sullissivik.designguiden.dk/website">kan findes her.</a></p>
+
+        <h2>Typografi</h2>
+
+        <div class="color--background--light">
+            <h1>Heading 1 - Bold 24pt/30pt</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <p>Body/regular 16pt/24pt</p>
+            <ul>
+                <li>Bullet - regular 16pt/24pt</li>
+                <li>Bullet - regular 16pt/24pt</li>
+                <li>Bullet - regular 16pt/24pt</li>
+            </ul>
+            <ol>
+                <li>Number - regular 16pt/24pt</li>
+                <li>Number - regular 16pt/24pt</li>
+                <li>Number - regular 16pt/24pt</li>
+            </ol>
+            <h1><a href="">Large link - header</a></h1>
+            <a class="h1" href="">Large link - not header</a>
+            <a href="">Small link</a>
+        </div>    
+
+        <div class="color--header" style="padding: 2rem; margin: 2rem 0;">
+            <h1>Heading 1 - Bold 24pt/30pt</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <p>Body/regular 16pt/24pt</p>
+            <ul>
+                <li>Bullet - regular 16pt/24pt</li>
+                <li>Bullet - regular 16pt/24pt</li>
+                <li>Bullet - regular 16pt/24pt</li>
+            </ul>
+            <ol>
+                <li>Number - regular 16pt/24pt</li>
+                <li>Number - regular 16pt/24pt</li>
+                <li>Number - regular 16pt/24pt</li>
+            </ol>
+            <h1><a href="">Large link - header</a></h1>
+            <a class="h1" href="">Large link - not header</a>
+            <a href="">Small link</a>
+        </div>
 
         <h2>Formularer</h2>
 
         <form @submit.prevent="sendFormRequest()">
 
+            <h3>Inputfelter</h3>
             <fieldset>
-                <label for="inputA">{{ $t('inputa') }}</label>
-                <input id="inputA" type="text" name="a" placeholder="test test" v-model="value_a">
-                <label for="inputB">{{ $t('inputb') }}</label>
-                <input id="inputB" type="text" name="b" v-model="value_b">
+                <label for="inputA">Inputfelt</label>
+                <input id="inputA" type="text" placeholder="Tips til at udfylde">
+                <label for="inputA">Inputfelt</label>
+                <input id="inputA" type="text" value="Præudfyldt værdi">
+                <label for="inputB">Dato-inputfelt</label>
+                <input id="inputB" type="date">
             </fieldset>
 
+            <h3>Knapper</h3>
             <fieldset>
-                <input type="submit" :value="$t('send')">
+                <input type="submit" value="Primary CTA">
+                <button>Secondary CTA</button>
             </fieldset>
 
         </form>
 
+        <h2>Tags</h2>
+
+        <h3>Store tags</h3>
+        <p>
+            <a class="tag-large" href="#">Hover</a>
+            <a class="tag-large" href="#">Bopælsattest</a>
+            <a class="tag-large" href="#">NEMID</a>
+            <a class="tag-large" href="#">Skattekort</a>
+            <a class="tag-large" href="#">Pas</a>
+            <a class="tag-large" href="#">Boligsikring</a>
+            <a class="tag-large" href="#">Selvangivelse</a>
+        </p>
+
+        <h3>Små tags</h3>
+        <p>
+            <a class="tag-small" href="#">Arbejdsmarked</a>
+            <a class="tag-small" href="#">Kurser</a>
+            <a class="tag-small" href="#">Straffeattest</a>
+            <a class="tag-small" href="#">Offentlig ansat</a>
+            <a class="tag-small" href="#">Overenskomstgrupper</a>
+        </p>
+
         <h2>Tiles</h2>
 
         <div style="display: flex; padding: 2rem; background-color: #eaecee;">
-            <div style="margin: 2rem;">
+            <div style="margin: 1rem;">
 
                 <h3>Tile type A</h3>
 
@@ -36,27 +107,27 @@
                 </a>
 
             </div>
-            <div style="margin: 2rem;">
+            <div style="margin: 1rem;">
 
                 <h3>Tile type B</h3>
 
                 <div class="tile-b">
                     <a href="#" class="tile-b--header">
-                        <img class="tile-b--icon" src="/static/img/SVG/sullisivk_erhverv.svg" alt="">
+                        <img class="tile-b--icon" src="/static/img/SVG/sullisivik_skatter-afgifter-og-gaeld_dark.svg" alt="">
                         <span class="tile-b--headertext">Ineqarneq nuunnerlu</span>
                     </a>
                     <nav class="tile-b--categories">
-                        <a href="#" class="tile-b--category">Arbejdsmarked</a>
-                        <a href="#" class="tile-b--category">Kurser</a>
-                        <a href="#" class="tile-b--category">Straffeattest</a>
-                        <a href="#" class="tile-b--category">Offentlig ansat</a>
-                        <a href="#" class="tile-b--category">Overenskomstgrupper</a>
+                        <a href="#" class="tag-small">Arbejdsmarked</a>
+                        <a href="#" class="tag-small">Kurser</a>
+                        <a href="#" class="tag-small">Straffeattest</a>
+                        <a href="#" class="tag-small">Offentlig ansat</a>
+                        <a href="#" class="tag-small">Overenskomstgrupper</a>
                     </nav>
                     <a class="tile-b--more">Se alle</a>
                 </div>
 
             </div>
-            <div style="margin: 2rem;">
+            <div style="margin: 1rem;">
 
                 <h3>Tile type C</h3>
 
@@ -69,6 +140,72 @@
                 </a>
 
             </div>
+        </div>
+
+        <h2>Dropdown handle</h2>
+
+        <div style="padding: 2rem; background-color: #eaecee;">
+            <button class="dropdown">
+                <span>Dropdown static</span>
+                <img class="dropdown--arrow" src="/static/img/SVG/sullisivk_arrow.svg" alt="">
+            </button>
+        </div>
+
+        <h2>Ikoner</h2>
+        <p>
+            <img class="icon" src="/static/img/SVG/sullisivk_arrow.svg" alt="">
+        </p>
+        <p>
+            <img class="icon" src="/static/img/SVG/sullisivk_close.svg" alt="">
+        </p>
+        <p>
+            <img class="icon" src="/static/img/SVG/sullisivk_search-small.svg" alt="">
+        </p>
+        <p>
+            <img class="icon" src="/static/img/SVG/sullisivk_Search-large.svg" alt="">
+        </p>
+        <p>
+            <img class="icon" src="/static/img/SVG/sullisivk_login.svg" alt="">
+        </p>
+
+        <h2>Farver</h2>
+
+        <div class="colorswatches">
+
+            <div class="color--header">
+                .color--header
+            </div>
+            <div class="color--header--dark">
+                .color--header--dark
+            </div>
+            <div class="color--header--light">
+                .color--header--light
+            </div>
+            <div class="color--text">
+                .color--text
+            </div>
+            <div class="color--link">
+                .color--link
+            </div>
+            <div class="color--link--hover">
+                .color--link--hover
+            </div>
+            <div class="color--background">
+                .color--background
+            </div>
+            <div class="color--background--light">
+                .color--background--light
+            </div>
+            <div class="color--background--dark">
+                .color--background--dark
+            </div>
+            <div class="color--cta">
+                .color--cta
+            </div>
+            <div class="color--cta--light">
+                .color--cta--light
+            </div>
+
         </div>
 
     </article>
@@ -103,6 +240,16 @@
 
 <style>
 
+    .colorswatches {
+        display: flex;
+        flex-flow: row wrap;
+    }
+
+    .colorswatches > div {
+        flex: 1 0 13rem;
+        height: 5rem;
+        padding: 1rem;
+    }
 
 </style>
 
@@ -110,14 +257,10 @@
 
     {
         "da": {
-            "title": "Designguide",
-            "forms": "Formularer",
-            "tiles": "Tiles"
+            "title": "Designguide"
         },
         "kl": {
-            "title": "Designguide - oversættelse mangler",
-            "forms": "Formularer - oversættelse mangler",
-            "tiles": "Tiles - oversættelse mangler"
+            "title": "Designguide - oversættelse mangler"
         }
     }
 
