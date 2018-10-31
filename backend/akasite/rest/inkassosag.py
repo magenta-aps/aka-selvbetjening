185c9523c84cae1f81f29c6d7e2413700ec3b594
Jakob <jakob@magenta.dk>
2018-10-31 15:02:47 +0100

    Changed validation in Inkassosag to new system

--- backend/akasite/rest/inkassosag.py
+++ backend/akasite/rest/inkassosag.py
@@ -8,115 +8,126 @@ import re
 
 # Internal tools
 from akasite.rest.base import JSONRestView
-from akasite.rest.validation import JsonValidator
+from akasite.rest.validation import JsonValidator, Error, Success
+from akasite.helpers.sharedfiles import getSharedJson
 
 logger = logging.getLogger(__name__)
 
-
-class FordringsException(Exception):
-    def __init__(self, message):
-        super(FordringsException, self).__init__(message)
-
-
 class InkassoSag(JSONRestView):
-    def get(self, request, *args, **kwargs):
-        dummyresponse = {"serversays": "Hello. You said InkassoSag/GET"}
+    '''This class handles the REST interface at /inkassosag
 
-        return HttpResponse(json.dumps(dummyresponse),
-                            content_type=JSONRestView.CT1)
+    It implements a POST endpoint. It should not be called directly,
+    but is instead called by Django's url handler.
+
+    '''
 
     def post(self, request, *args, **kwargs):
+        '''
+        Method for POST handler at /inkassosag
+
+        :param request: The request.
+        :type request: HttpRequest.
+        :returns: HttpResponse, HttpResponseBadRequest
+
+        '''
         baseresponse = super().postfile(request)
 
         if baseresponse.status_code == 200:
 
             logger.debug(self.payload)
-            validation1 = validateInkassoJson(self.payload['POST'])
-            if validation1 == []:
-                validationStatus = validateFordringsgrupper(self.payload['POST'])
-            else:
-                validationStatus = validation1
+            return validateInkassoJson(self.payload['POST']
+                ).andThen(validateFordringsgrupper
+                ).toHttpResponse()
 
-            return HttpResponse(json.dumps(validationStatus),
-                                content_type=JSONRestView.CT1)
         else:
             return baseresponse
 
 
-def validateInkassoJson(reqJson):
-    jsonSchema = {
-            'type': 'object',
-            'properties': {
-                'fordringshaver':   {'type': 'string'},
-                'debitor':          {'type': 'string'},
-                'fordringshaver2':  {'type': 'string'},
-                'fordringsgruppe':  {'type': 'integer'},
-                'fordringstype':    {'type': 'integer'}
+jsonSchema = {
+        'type': 'object',
+        'properties': {
+            'fordringshaver':   {'type': 'string'},
+            'debitor':          {'type': 'string'},
+            'fordringshaver2':  {'type': 'string'},
+            'fordringsgruppe':  {
+                'type': 'string',
+                'pattern': '[0-9]+'
                 },
-            'required': ['fordringshaver',
-                         'debitor',
-                         'fordringsgruppe',
-                         'fordringstype']
-            }
+            'fordringstype':    {'type': 'string'}
+            },
+        'required': ['fordringshaver',
+                     'debitor',
+                     'fordringsgruppe',
+                     'fordringstype']
+        }
+def validateInkassoJson(reqJson):
+    '''Validate a dict data-structure for the /inkassosag endpoint
+
+    :param reqJson: The Json to be validated
+    :type reqJson: Dict
+    :returns: Error, Success
+
+    '''
 
     return JsonValidator(jsonSchema).validate(reqJson)
 
 
 def validateFordringsgrupper(reqJson):
+    '''Validate that a given request's fordrings-gruppe and type is valid
+
+    :param reqJson: The Json to be validated
+    :type reqJson: Dict
+    :returns: Error, Success
+
+    '''
     try:
-        fordringsJson = getSharedJson('fordringsgruppe.json')
-        fordringsgruppe = getOnlyElement(fordringsJson,
-                                         reqJson['fordringsgruppe'])
-        if fordringsgruppe['status']:
-            fordringstype = getOnlyElement(fordringsgruppe['elem']['sub_groups'],
-                                           reqJson['fordringstype'])
-            if fordringstype['status']:
-                return {'status': True}
-            else:
-                return fordringstype
-        else:
-            return fordringsgruppe
+        fordringJson = getSharedJson('fordringsgruppe.json')
+        fordringsgruppe = getOnlyElement(fordringJson,
+                reqJson['fordringsgruppe'], 'fordringsgruppe')
+        return fordringsgruppe.andThen(
+                lambda e:
+                    getOnlyElement(e['sub_groups'], reqJson['fordringstype'],
+                        'fordringstype')
+                )
     except Exception as e:
-        logger.warning("Invalid JSON recieved:"+str(reqJson)+"\n\nException: "+e)
-        return {
-                    'status': False,
-                    'msg': 'fordringsgruppe or fordringstype missing or NaN'
-               }
+        logger.warning("Invalid JSON recieved:" + str(reqJson) +
+                "\n\nException: " + str(e))
+        return Error('fordringsgruppe_or_type_problem')
 
 
-def getOnlyElement(l, fordring):
-    fordringsList = [x for x in l if x['id'] == fordring]
+def getOnlyElement(l, fid, fordringsName):
+    '''Get 1 element, if only one exists, otherwise an Error is returned
+
+    Finds exactly one element(Dict), with a field `id` which has the same
+    value as `fid`, if no element matches, or multiple elements matches
+    an Error is instead returned.
+    Because it can return an error, it returns a Success(Dict) instead
+    of just the Dict with the given element.
+    An easy way to use the Dict, upon success is using Success.andThen.
+    See :func:`~akasite.rest.validation.Success.andThen`
+
+    :param l: The list to look for the element in
+    :type l: List
+    :param fid: The id to look for
+    :type fid: String
+    :param fordringsName: The name of the fordring(used for the errors)
+    :type fordringsName: String
+    :returns: Success(Dict), Error
+
+    '''
+    fordringsList = [x for x in l if x['id'] == fid]
     if len(fordringsList) < 1:
-        logger.error("The following list:\n" + str(l) + "\n was expected to " +
-                     "have 1 element with the following id: " + fordring +
-                     ", but none was found.\n" +
-                     "The error might be a user error, if a custom " +
-                     "REST-client was used")
-        return {
-                    'status': False,
-                    'field': 'fordringsgruppe',
-                    'msg': str.format(str(e), 'fordringsgruppe')
-               }
+        logger.warning("The following list:\n" + str(l) + "\n was expected to " +
+                       "have 1 element with the following id: " + fid +
+                       ", but none was found.\n" +
+                       "The error might be a user error, if a custom " +
+                       "REST-client was used")
+        return Error(fordringsName + '_not_found', fordringsName)
 
     elif len(fordringsList) > 1:
         logger.error("The following list:\n" + str(l) + "\n was only " +
                      "expected to have 1 element with the following id: " +
-                     str(fordring) + ", but multiple elements were found")
-        return {
-                    'status': False,
-                    'field': 'fordringstype',
-                    'msg': str.format(str(e), 'fordringstype')
-               }
+                     str(fid) + ", but multiple elements were found")
+        return Error('multiple_' + fordringsName + '_found', fordringsName)
     else:
-        return {'status': True, 'elem': fordringsList[0]}
-
-
-def getSharedJson(fileName):
-    """
-    This function generates a json(can be used as a dict)
-    from a file shared between the frontend and backend.
-    The file must be in a valid json format.
-
-    """
-    with open('../shared/'+fileName, 'r') as jsonfile:
-        return json.loads(jsonfile.read())
+        return Success(fordringsList[0])
