35bde29c6723554fd2c6a60b009ecc4becc91f2b
William Heyman Krill <whk@magenta.dk>
2018-11-13 15:34:48 +0100

    Upload multiple files and fix meddebitorer

--- frontend/src/components/indberet_fordring/IndberetFordring.vue
+++ frontend/src/components/indberet_fordring/IndberetFordring.vue
@@ -50,27 +50,23 @@
                 -->
             </fieldset>
 
-            <!--TODO: Allow multiple files and show file list-->
-            <!--
-               This is actually quite easy. Add the "multiple" attribute  to the input element.
-               Then use getFileData to extract the list of files.
-               The template should magically display it if you add something like
-               ```
-                   <table v-if="files">
-                     <tr v-for="(f, index) in files" :key="index">
-                       <td>{{ f.name }}</td>
-                       <td>{{ f.size }} kB</td>
-                     </tr>
-                   </table>
-               ```
-               Maybe iterating over af Filelist like this will cause you problems.
-               Then you should convert it to an array in getFileData.
-               See https://developer.mozilla.org/en-US/docs/Web/API/FileList
-               and https://developer.mozilla.org/en-US/docs/Web/API/File for more info on working with files.
-            -->
-            <fieldset>
-                <input type="file" :class="{submitted: isSubmitted}" @change="getFileData($event.target.files)">
-            </fieldset>
+            <table>
+                <thead>
+                    <tr>
+                        <th>{{ $t('filnavn') }}</th>
+                        <th>{{ $t('stoerelse') }}</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody v-if="filer">
+                    <tr v-for="(f, index) in filer" :key="index">
+                        <td>{{ f.name }}</td>
+                        <td>{{ f.size }} kB</td>
+                        <td><a @click="deleteFile(index)">{{ $t('slet') }}</a></td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="file" multiple @change="selectFiles($event.target.files)">
 
             <div style="display: flex; flex-flow: row wrap;">
                 <fieldset>
@@ -106,69 +102,69 @@
 
             <fieldset>
                 <label id="lbl_barns_cpr"
-                       for="tb_barns_cpr">{{ $t('barns_cpr') }}</label>
-                <input id="tb_barns_cpr"
+                       for="barns_cpr">{{ $t('barns_cpr') }}</label>
+                <input id="barns_cpr"
                        type="text"
                        :class="{submitted: isSubmitted}"
                        v-model="barns_cpr"
                        minlength="10"
                        maxlength="10">
 
-                <label id="lbl_ekstern_sagsnummer" for="tb_ekstern_sagsnummer">{{ $t('ekstern_sagsnummer') }}</label>
-                <input id="tb_ekstern_sagsnummer" type="text" :class="{submitted: isSubmitted}" v-model="ekstern_sagsnummer" required>
+                <label id="lbl_ekstern_sagsnummer" for="ekstern_sagsnummer">{{ $t('ekstern_sagsnummer') }}</label>
+                <input id="ekstern_sagsnummer" type="text" :class="{submitted: isSubmitted}" v-model="ekstern_sagsnummer" required>
 
-                <label id="lbl_fakturanr" for="tb_fakturanr">{{ $t('fakturanr') }}</label>
-                <input id="tb_fakturanr" type="text" :class='{submitted: isSubmitted}' v-model="fakturanr" required>
+                <label id="lbl_fakturanr" for="fakturanr">{{ $t('fakturanr') }}</label>
+                <input id="fakturanr" type="text" :class='{submitted: isSubmitted}' v-model="fakturanr" required>
 
-                <label id="lbl_bnr" for="tb_bnr">{{ $t('bnr') }}</label>
-                <input id="tb_bnr" type="text" :class="{submitted: isSubmitted}" v-model="bnr">
+                <label id="lbl_bnr" for="bnr">{{ $t('bnr') }}</label>
+                <input id="bnr" type="text" :class="{submitted: isSubmitted}" v-model="bnr">
             </fieldset>
 
             <fieldset> <!--TODO: Fix wrapping -->
-                <label id="lbl_hovedstol" for="tb_hovedstol">{{ $t('hovedstol') }}</label>
-                <input id="tb_hovedstol" type="text" :class="{submitted: isSubmitted}" v-model="hovedstol" required>
-                <label id="lbl_hovedstol_posteringstekst" for="tb_hovedstol_posteringstekst">{{ $t('posteringstekst') }}</label>
-                <input id="tb_hovedstol_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="hovedstol_posteringstekst" required>
+                <label id="lbl_hovedstol" for="hovedstol">{{ $t('hovedstol') }}</label>
+                <input id="hovedstol" type="text" :class="{submitted: isSubmitted}" v-model="hovedstol" required>
+                <label id="lbl_hovedstol_posteringstekst" for="hovedstol_posteringstekst">{{ $t('posteringstekst') }}</label>
+                <input id="hovedstol_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="hovedstol_posteringstekst" required>
 
-                <label id="lbl_bankrente" for="tb_bankrente">{{ $t('bankrente') }}</label>
-                <input id="tb_bankrente" type="text" :class="{submitted: isSubmitted}" v-model="bankrente">
-                <label id="lbl_bankrente_posteringstekst" for="tb_bankrente_posteringstekst">{{ $t('posteringstekst') }}</label>
-                <input id="tb_bankrente_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="bankrente_posteringstekst">
+                <label id="lbl_bankrente" for="bankrente">{{ $t('bankrente') }}</label>
+                <input id="bankrente" type="text" :class="{submitted: isSubmitted}" v-model="bankrente">
+                <label id="lbl_bankrente_posteringstekst" for="bankrente_posteringstekst">{{ $t('posteringstekst') }}</label>
+                <input id="bankrente_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="bankrente_posteringstekst">
 
-                <label id="lbl_bankgebyr" for="tb_bankgebyr">{{ $t('bankgebyr') }}</label>
-                <input id="tb_bankgebyr" type="text" :class="{submitted: isSubmitted}" v-model="bankgebyr">
-                <label id="lbl_bankgebyr_posteringstekst" for="tb_bankgebyr_posteringstekst">{{ $t('posteringstekst') }}</label>
-                <input id="tb_bankgebyr_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="bankgebyr_posteringstekst">
+                <label id="lbl_bankgebyr" for="bankgebyr">{{ $t('bankgebyr') }}</label>
+                <input id="bankgebyr" type="text" :class="{submitted: isSubmitted}" v-model="bankgebyr">
+                <label id="lbl_bankgebyr_posteringstekst" for="bankgebyr_posteringstekst">{{ $t('posteringstekst') }}</label>
+                <input id="bankgebyr_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="bankgebyr_posteringstekst">
 
-                <label id="lbl_rente" for="tb_rente">{{ $t('rente') }}</label>
-                <input id="tb_rente" type="text" :class="{submitted: isSubmitted}" v-model="rente">
-                <label id="lbl_rente_posteringstekst" for="tb_rente_posteringstekst">{{ $t('posteringstekst') }}</label>
-                <input id="tb_rente_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="rente_posteringstekst">
+                <label id="lbl_rente" for="rente">{{ $t('rente') }}</label>
+                <input id="rente" type="text" :class="{submitted: isSubmitted}" v-model="rente">
+                <label id="lbl_rente_posteringstekst" for="rente_posteringstekst">{{ $t('posteringstekst') }}</label>
+                <input id="rente_posteringstekst" type="text" :class="{submitted: isSubmitted}" v-model="rente_posteringstekst">
             </fieldset>
 
             <fieldset> <!--TODO: Fix wrapping -->
-                <label id="lbl_periodestart" for="tb_periodestart">{{ $t('periodestart') }}</label>
-                <input id="tb_periodestart" type="date" :class="{submitted: isSubmitted}" v-model="periodestart">
+                <label id="lbl_periodestart" for="periodestart">{{ $t('periodestart') }}</label>
+                <input id="periodestart" type="date" :class="{submitted: isSubmitted}" v-model="periodestart">
 
-                <label id="lbl_periodeslut" for="tb_periodeslut">{{ $t('periodeslut') }}</label>
-                <input id="tb_periodeslut" type="date" :class="{submitted: isSubmitted}" v-model="periodeslut">
+                <label id="lbl_periodeslut" for="periodeslut">{{ $t('periodeslut') }}</label>
+                <input id="periodeslut" type="date" :class="{submitted: isSubmitted}" v-model="periodeslut">
 
-                <label id="lbl_forfaldsdato" for="tb_forfaldsdato">{{ $t('forfaldsdato') }}</label>
-                <input id="tb_forfaldsdato" type="date" :class="{submitted: isSubmitted}" v-model="forfaldsdato" required>
+                <label id="lbl_forfaldsdato" for="forfaldsdato">{{ $t('forfaldsdato') }}</label>
+                <input id="forfaldsdato" type="date" :class="{submitted: isSubmitted}" v-model="forfaldsdato" required>
 
-                <label id="lbl_betalingsdato" for="tb_betalingsdato">{{ $t('betalingsdato') }}</label>
-                <input id="tb_betalingsdato" type="date" :class="{submitted: isSubmitted}" v-model="betalingsdato" required>
+                <label id="lbl_betalingsdato" for="betalingsdato">{{ $t('betalingsdato') }}</label>
+                <input id="betalingsdato" type="date" :class="{submitted: isSubmitted}" v-model="betalingsdato" required>
 
-                <label id="lbl_foraeldelsesdato" for="tb_foraeldelsesdato">{{ $t('foraeldelsesdato') }}</label>
-                <input id="tb_foraeldelsesdato" type="date" :class="{submitted: isSubmitted}" v-model="foraeldelsesdato" required>
+                <label id="lbl_foraeldelsesdato" for="foraeldelsesdato">{{ $t('foraeldelsesdato') }}</label>
+                <input id="foraeldelsesdato" type="date" :class="{submitted: isSubmitted}" v-model="foraeldelsesdato" required>
             </fieldset>
 
             <fieldset>
-                <label id="lbl_kontaktperson" for="tb_kontaktperson">{{ $t('kontaktperson') }}</label>
-                <input id="tb_kontaktperson" type="text" :class="{submitted: isSubmitted}" v-model="kontaktperson">
+                <label id="lbl_kontaktperson" for="kontaktperson">{{ $t('kontaktperson') }}</label>
+                <input id="kontaktperson" type="text" :class="{submitted: isSubmitted}" v-model="kontaktperson">
 
-                <label id="lbl_noter" for="tb_noter">{{ $t('noter') }}</label>
-                <input id="tb_noter" type="text" :class="{submitted: isSubmitted}" v-model="noter">
+                <label id="lbl_noter" for="noter">{{ $t('noter') }}</label>
+                <input id="noter" type="text" :class="{submitted: isSubmitted}" v-model="noter">
             </fieldset>
 
             <fieldset>
@@ -217,7 +213,8 @@ export default {
       fordringshaver2: null,
       fordringsgruppe: null,
       fordringstype: null,
-      fil: null, /* TODO: make this a list of files */
+      filer: [],
+      valgte_filer: [],
       barns_cpr: null,
       ekstern_sagsnummer: null,
       fakturanr: null,
@@ -250,7 +247,6 @@ export default {
         'fordringshaver2',
         'fordringsgruppe',
         'fordringstype',
-        'fil',
         'barns_cpr',
         'ekstern_sagsnummer',
         'fakturanr',
@@ -318,15 +314,21 @@ export default {
     },
     getCSRFToken: function () {
       this.csrftoken = document.cookie.replace(
-        /(?:(?:^|.*;\s*)csrftoken\s*\=\s*([^;]*).*$)|^.*$/,
+        /(?:(?:^|.*;\s*)csrftoken\s*=\s*([^;]*).*$)|^.*$/,
         '$1'
       )
     },
-    getFileData: function (files) {
-      this.fil = files[0]
+    selectFiles: function (files) {
+      for (var i = 0; i < files.length; i++) {
+        this.filer.push(files[i])
+      }
+    },
+    deleteFile: function (index) {
+      this.filer.splice(index, 1)
     },
     fetchFormData: function () {
       let formdata = new FormData()
+
       let that = this
       function appendData (string) {
         if (that[string] !== null) {
@@ -340,10 +342,16 @@ export default {
 
       this.form_fields.forEach(appendData)
 
+      this.filer.forEach(function (fil, i) {
+        let idx = i + 1
+        formdata.append('fil' + idx, fil)
+      })
+
       this.meddebitorer.forEach(function (meddebitor, i) {
         let idx = i + 1
-        if (!(meddebitor.cpr === '' || meddebitor.cvr === '')) {
+        if (meddebitor.cpr !== '') {
           formdata.append('meddebitor' + idx + '_cpr', meddebitor.cpr)
+        } else if (meddebitor.cvr !== '') {
           formdata.append('meddebitor' + idx + '_cvr', meddebitor.cvr)
         }
       })
@@ -384,11 +392,17 @@ export default {
         border: 2px solid #D7404D;
     }
     .submitted:invalid {
-        border: 2px solid #D7404D;
+       border: 2px solid #D7404D;
     }
     input[disabled] {
         background-color: #d6dbde;
-   }
+    }
+    table {
+       border-collapse: collapse;
+    }
+    tr {
+        border-bottom: 1px solid #ddd;
+    }
 </style>
 
 <i18n>
@@ -401,6 +415,9 @@ export default {
       "debitor": "Debitor",
       "fordringsgruppe": "Ekstern fordringsgruppe",
       "fordringstype": "Ekstern fordringstype",
+      "filnavn": "Filnavn",
+      "stoerelse": "Størrelse",
+      "slet": "Slet",
       "barns_cpr": "Barns CPR-nr",
       "ekstern_sagsnummer": "Ekstern sagsnummer",
       "fakturanr": "Fakturanr",
@@ -427,6 +444,9 @@ export default {
       "debitor": "Akiligassalik",
       "fordringsgruppe": "Akiitsoqarfimmiit suliassiisutip ataatsimooruffiata suussusaa",
       "fordringstype": "Akiitsoqarfimmiit suliasiissutit suussusaa",
+      "filnavn": "Fil-ip atia",
+      "stoerelse": "Imartussuseq",
+      "slet": "Nunguteruk",
       "barns_cpr": "Meeqqap inuup normua",
       "ekstern_sagsnummer": "Suliassiissutip akiitsoqarfimmiit normua",
       "fakturanr": "MANGLER",
