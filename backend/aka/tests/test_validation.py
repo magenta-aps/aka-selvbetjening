78052a3bf821bea1ece211bfb73ba3d64463ad62
Jakob <jakob@magenta.dk>
2019-03-27 13:03:54 +0100

    Basic test cases for date ordering validation

--- backend/aka/tests/test_validation.py
+++ backend/aka/tests/test_validation.py
@@ -5,3 +5,9 @@ from datetime import date
 
 
 class ValidationTestCase(SimpleTestCase):
+    def test_validate_not_after(self):
+        # Base cases
+        self.assertTrue( validateNotAfter(date(2002,12,12), date(2003,12,12)).status )
+        self.assertTrue( validateNotAfter(date(2002,12,12), date(2002,12,12)).status )
+        self.assertFalse( validateNotAfter(date(2003,12,12), date(2002,12,12)).status )
+        
