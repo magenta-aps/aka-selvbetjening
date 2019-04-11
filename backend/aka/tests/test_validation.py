e3cc1dcd2367ffd49ee65d6b9290bb680196ef8a
Jakob <jakob@magenta.dk>
2019-04-11 11:39:41 +0200

    Updated and added tests for periodestart/slut validation

--- backend/aka/tests/test_validation.py
+++ backend/aka/tests/test_validation.py
@@ -1,13 +1,10 @@
 from django.test import SimpleTestCase
 from aka.helpers.result import Success, Error 
-from aka.helpers.validation import validateRequired, validateNotAfter
+from aka.helpers.validation import validateRequired
 from datetime import date
 
 
 class ValidationTestCase(SimpleTestCase):
-    def test_validate_not_after(self):
-        # Base cases
-        self.assertTrue( validateNotAfter(date(2002,12,12), date(2003,12,12)).status )
-        self.assertTrue( validateNotAfter(date(2002,12,12), date(2002,12,12)).status )
-        self.assertFalse( validateNotAfter(date(2003,12,12), date(2002,12,12)).status )
-        
+    def test_validateRequired(self):
+        self.assertTrue(validateRequired(['1'], {'1':1}).status)
+        self.assertFalse(validateRequired(['1'], {'2':2}).status)
