2255fcc17c72b085615ca39b40888d6b1c990466
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2019-06-24 09:45:56 +0200

    fix

--- backend/openid/views.py
+++ backend/openid/views.py
@@ -4,19 +4,20 @@ from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
+from jwkest.jwk import rsa_load
 from oic.oauth2 import ErrorResponse
 from oic.oic import Client, rndstr
 from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 from django.conf import settings
 from oic.oic.message import AuthorizationResponse, RegistrationResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
-from oic.utils.keyio import keybundle_from_local_file, KeyBundle, KeyJar
+from oic.utils.keyio import KeyBundle
 
 open_id_settings = settings.OPENID_CONNECT
 logger = logging.getLogger(__name__)
-kj = KeyJar()
-kb = KeyBundle(source='file://{}'.format(open_id_settings['private_key']), fileformat='der')
-kj.add_kb(issuer='https://loginqa.sullissivik.gl', kb=kb)
+key = rsa_load(open_id_settings['private_key'])
+kc_rsa = KeyBundle([{'key': key, 'kty': 'RSA', 'use': 'ver'},
+                    {'key': key, 'kty': 'RSA', 'use': 'sig'}])
 
 
 class Login(View):
@@ -63,7 +64,8 @@ class Callback(View):
             del request.session['oid_nonce']
 
         client_cert = (settings.OPENID_CONNECT['client_certificate'], settings.OPENID_CONNECT['private_key'])
-        client = Client(client_authn_method=CLIENT_AUTHN_METHOD, client_cert=client_cert, keyjar=kj)
+        client = Client(client_authn_method=CLIENT_AUTHN_METHOD, client_cert=client_cert)
+        client.keyjar[""] = kc_rsa
 
         client_configuration = {'client_id': settings.OPENID_CONNECT['client_id'],
                                 'token_endpoint_auth_method': 'private_key_jwt'}
@@ -89,13 +91,13 @@ class Callback(View):
 
             request_args = {'code': aresp['code'],
                             'redirect_uri': request.build_absolute_uri(reverse('openid:callback'))}
-            client.keyjar.add()
 
             # this just needs to be the same as the previous callback
             resp = client.do_access_token_request(state=aresp['state'],
                                                   scope=settings.OPENID_CONNECT['scope'],
-                                                  request_args=request_args
-                                                  #authn_method="client_secret_jwt",
+                                                  request_args=request_args,
+                                                  authn_method="private_key_jwt",
+                                                  authn_endpoint='token'
                                                   )
 
             logger.debug('access token responds', resp.status_code)
