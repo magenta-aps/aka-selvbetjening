5c37926a6811040bb6c5212c4bd05c97c629aa7c
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2019-06-24 11:05:49 +0200

    removed error page

--- backend/openid/urls.py
+++ backend/openid/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import include, url
-from openid.views import Callback, Login, Logout, ErrorPage
+from openid.views import Callback, Login, Logout
 
 urlpatterns = [
     url(r'^callback/$', Callback.as_view(), name='callback'),
     url(r'^login/$', Login.as_view(), name='login'),
     url(r'^logout/$', Logout.as_view(), name='logout'),
-    url(r'^error/$', ErrorPage.as_view(), name='error'),
 ]
