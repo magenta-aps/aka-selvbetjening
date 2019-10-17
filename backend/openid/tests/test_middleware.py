10917f3536494adbeee526a52b6982b0d968eeaf
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2019-10-17 09:19:48 +0200

    openid: added comment to test

--- backend/openid/tests/test_middleware.py
+++ backend/openid/tests/test_middleware.py
@@ -8,11 +8,17 @@ class MiddlewareTestCase(TestCase):
         pass
 
     def test_not_logged_in(self):
+        """
+        ensure we cant reach the vu js app when not logged in
+        """
         r = self.client.get(reverse('index'), follow=False)
         self.assertEqual(r.status_code, 302)
         self.assertEqual(r.url, reverse('openid:login'))
 
     def test_not_logged_in_form_view(self):
+        """
+        ensure we cant do a form post when not logged in, we should get redirected to the login page instead.
+        """
         data = {'fordringshaver': 'firstname', 'debitor': 'debitor'}
         r = self.client.post(reverse('inkassosag'), data=data)
         self.assertEqual(r.status_code, 302)
