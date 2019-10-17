3d1f1d7bb6d28e8aad6d2d76dd571fb757eac96e
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2019-10-17 09:09:45 +0200

    openid: tests for middle to ensure redirection works

---
+++ backend/openid/tests/test_middleware.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, override_settings
+from django.urls import reverse
+
+
+@override_settings(OPENID_CONNECT={'enabled': True})
+class MiddlewareTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_not_logged_in(self):
+        r = self.client.get(reverse('index'), follow=False)
+        self.assertEqual(r.status_code, 302)
+        self.assertEqual(r.url, reverse('openid:login'))
+
+    def test_not_logged_in_form_view(self):
+        data = {'fordringshaver': 'firstname', 'debitor': 'debitor'}
+        r = self.client.post(reverse('inkassosag'), data=data)
+        self.assertEqual(r.status_code, 302)
+        self.assertEqual(r.url, reverse('openid:login'))
+
