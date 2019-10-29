1e53c00ccf6c5d1cfc5dfb61a9028e00745002ff
Lars Peter Thomsen <larsp@magenta.dk>
2019-10-29 10:03:17 +0100

    Fix refactoring issues with /aka/

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../backend/aka/helpers'))
+sys.path.insert(0, os.path.abspath('../backend/aka/clients'))
 sys.path.insert(0, os.path.abspath('../backend/aka'))
 sys.path.insert(0, os.path.abspath('../backend'))
 
