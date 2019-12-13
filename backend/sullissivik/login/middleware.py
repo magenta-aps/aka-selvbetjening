99b1e7337251dedebf05567a60dacda2d57d4f72
Lars Peter Thomsen <larsp@magenta.dk>
2019-12-13 10:30:21 +0100

    Whitelist favicon and language resources

--- backend/sullissivik/login/middleware.py
+++ backend/sullissivik/login/middleware.py
@@ -14,25 +14,31 @@ class LoginManager:
 
     white_listed_urls = []
 
-    @staticmethod
-    def register_whitelist(*paths):
-        for path in paths:
-            LoginManager.white_listed_urls.append(path)
-
     def __init__(self, get_response):
         self.get_response = get_response
-        self.white_listed_urls = [reverse_lazy('aka:login')] + NemId.whitelist + OpenId.whitelist
+
+        # Urls that should not redirect an anonymous user to login page
+        self.white_listed_urls = NemId.whitelist + OpenId.whitelist + [
+            reverse_lazy('aka:login'),
+            '/favicon.ico',
+            reverse_lazy('aka:javascript-language-catalog', kwargs={'locale': 'da'}),
+            reverse('aka:javascript-language-catalog', kwargs={'locale': 'kl'}),
+        ]
+        print(self.white_listed_urls)
 
     def __call__(self, request):
         if self.enabled:
             # When any non-whitelisted page is loaded, check if we are authenticated
             if request.path not in self.white_listed_urls:
+                print(request.path+" is not in "+str(self.white_listed_urls))
                 if 'user_info' not in request.session or not request.session['user_info']:
                     if not self.authenticate(request):  # The user might not have anything in his session, but he may have a cookie that can log him in anyway
                         backpage = urlquote(request.path)
                         if request.GET:
                             backpage += "?" + urlencode(request.GET, True)
                         return redirect(reverse_lazy('aka:login') + "?back=" + backpage)
+            else:
+                print(request.path+" is in "+str(self.white_listed_urls))
         return self.get_response(request)
 
     @staticmethod
