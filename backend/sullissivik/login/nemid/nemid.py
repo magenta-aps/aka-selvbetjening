05514d4e582f4a635c6107b99f5ec0286f4255d8
Lars Peter Thomsen <larsp@magenta.dk>
2019-12-13 10:31:44 +0100

    Donøt fall over if nemid SOAP can't be accessed

--- backend/sullissivik/login/nemid/nemid.py
+++ backend/sullissivik/login/nemid/nemid.py
@@ -15,23 +15,26 @@ class NemId:
     def authenticate(request):
         user = SessionOnlyUser.get_user(request.session)
         if not user.is_authenticated:
-            config = settings.NEMID_CONNECT
-            cookie = request.COOKIES.get(config.get('cookie_name'))
-            if cookie is not None:
-                client = Client(config.get('federation_service'))
-                # Convert zeep object to OrderedDict
-                user_data = serialize_object(client.service.GetUser(cookie))
-                # federationPid = user_data.get('FederationPid')
-                cpr = user_data.get('CPR')
-                name = user_data.get('Name')
-                is_authenticated = user_data.get('IsAuthenticated')
-                if is_authenticated is not True:
-                    return None
-                if cpr is None:
-                    return None
-                user = request.user = SessionOnlyUser.get_user(request.session, cpr, name)
-                if request.user.is_authenticated:
-                    request.session['user_info'] = user.dict()
+            try:
+                config = settings.NEMID_CONNECT
+                cookie = request.COOKIES.get(config.get('cookie_name'))
+                if cookie is not None:
+                    client = Client(config.get('federation_service'))
+                    # Convert zeep object to OrderedDict
+                    user_data = serialize_object(client.service.GetUser(cookie))
+                    # federationPid = user_data.get('FederationPid')
+                    cpr = user_data.get('CPR')
+                    name = user_data.get('Name')
+                    is_authenticated = user_data.get('IsAuthenticated')
+                    if is_authenticated is not True:
+                        return None
+                    if cpr is None:
+                        return None
+                    user = request.user = SessionOnlyUser.get_user(request.session, cpr, name)
+                    if request.user.is_authenticated:
+                        request.session['user_info'] = user.dict()
+            except:
+                pass
         return user
 
     whitelist = [reverse_lazy('nemid:login')]
