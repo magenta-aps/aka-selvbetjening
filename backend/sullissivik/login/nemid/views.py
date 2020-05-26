6a08cb4976e08aedc67d255424e0f1b847744a17
Lars Peter Thomsen <larsp@magenta.dk>
2020-05-26 17:41:45 +0200

    Preserve language on login redirect

--- backend/sullissivik/login/nemid/views.py
+++ backend/sullissivik/login/nemid/views.py
@@ -1,6 +1,10 @@
+from urllib.parse import urlparse, urlunparse
+
 from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
+from django.http import QueryDict
 from django.shortcuts import redirect
+from django.utils import translation
 from django.views import View
 from django.views.generic import TemplateView
 from sullissivik.login.middleware import LoginManager
@@ -29,8 +33,15 @@ class Login(View):
             return redirect(LoginManager.get_backpage(request))
 
         # Sullissivik redirects back here, so if login somehow fails (and we don't trust sullisivik to keep the user in that case), we re-check and direct back
+
+
+        login_url_parts = list(urlparse(self.config.get('login_url')))
+        querystring = QueryDict(login_url_parts[4], mutable=True)
+        querystring['sc_lang'] = settings.LOCALE_MAP.get(translation.get_language(), "da-DK")
+        login_url_parts[4] = querystring.urlencode(safe='/')
+
         return redirect_to_login(
             request.build_absolute_uri(),
-            self.config.get('login_url'),
+            urlunparse(login_url_parts),
             self.config.get('redirect_field')
         )
