4e7048a25e84495090a10f518b9f56e09483f3bc
Lars Peter Thomsen <larsp@magenta.dk>
2020-07-30 14:41:57 +0200

    [#36384] Number formatting

--- backend/aka/templatetags/aka_tags.py
+++ backend/aka/templatetags/aka_tags.py
@@ -83,4 +83,5 @@ def get(item, attribute):
 @register.filter
 def number(item):
     locale.setlocale(locale.LC_ALL, ('da_dk', 'utf-8'))
-    return locale.format("%.2f", float(item), grouping=True)
+    value = float(item.replace(',', '.')) if isinstance(item, str) else item
+    return locale.format("%.2f", value, grouping=True)
