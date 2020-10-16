bddc77a273945171b7a744a1760c0452d0f81c4e
Lars Peter Thomsen <larsp@magenta.dk>
2020-10-16 11:10:10 +0200

    Cleanup

--- backend/aka/encrypted_logging.py
+++ backend/aka/encrypted_logging.py
@@ -1,18 +1,16 @@
 import logging
+
 from django.conf import settings
 from gnupg import GPG
-import time
 
 
 def EncryptedLogFormatterFactory(format=None):
     return EncryptedLogFormatter(format)
 
-# NOTE TO SELF: registrér 3 timer på #25312 d. 08.10.2020
 
 class EncryptedLogFormatter(logging.Formatter):
 
     def __init__(self, format=None, datefmt=None):
-        # formatstring = '%(asctime)s %(levelname)s %(name)s %(pathname)s:%(lineno)s    %(message)s'
         self.gpg = GPG()
         super(EncryptedLogFormatter, self).__init__(fmt=format, datefmt=datefmt)
 
