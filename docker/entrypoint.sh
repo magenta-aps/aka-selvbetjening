5f744318f5c488ec93594b4931803f4db9182edb
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2021-09-29 15:29:33 +0200

    fixed docker file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-python manage.py wait_for_db
-echo 'running migations'
-python manage.py migrate
+SKIP_MIGRATIONS=${SKIP_MIGRATIONS:=false}
+if [ "$SKIP_MIGRATIONS" = false ]; then
+  python manage.py wait_for_db
+  echo 'running migations'
+  python manage.py migrate
+fi
 exec "$@"
