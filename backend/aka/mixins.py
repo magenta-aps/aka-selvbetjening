1fcc01bd9ce4f11ab934b18f9798218c2ef9e2a1
Lars Peter Thomsen <larsp@magenta.dk>
2021-10-07 15:57:48 +0200

    [#46183] Show downtime template up front when an error occurs, instead of redirecting

--- backend/aka/mixins.py
+++ backend/aka/mixins.py
@@ -138,7 +138,7 @@ class HasUserMixin(object):
             self.obtain_cvr(request)
             return super().dispatch(request, *args, **kwargs)
         except SSLError:
-            return redirect('aka:downtime')
+            return TemplateResponse('aka/downtime.html')
 
     def get_context_data(self, **kwargs):
         context = {
