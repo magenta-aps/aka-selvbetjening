4a6758739216095158998ef150956754595eb468
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2022-03-01 16:02:41 +0100

    removed more outdated stuff #48935

--- socks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Set up socks connection so locally running django can see the Prisme test server
-# Make sure you are connected to the VPN to access the server
-ssh -D 8888 10.240.76.38
