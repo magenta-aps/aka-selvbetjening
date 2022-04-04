dbc728c54effce799059b422ee124d45f62c5d2e
Lars Peter Thomsen <larsp@magenta-aps.dk>
2022-04-04 15:46:53 +0200

    Noticket repair test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 SKIP_MIGRATIONS=${SKIP_MIGRATIONS:=false}
+TEST=${TEST:=false}
 PGP_KEY=${PGP_KEY:=false}
 if [ "$SKIP_MIGRATIONS" = false ]; then
   python manage.py wait_for_db
   echo 'running migrations'
   python manage.py migrate
+fi
+if [ "$TEST" = true ]; then
+  echo 'running tests!'
+  python manage.py test
 fi
 if [ "$PGP_KEY" != false ]; then
   echo "$PGP_KEY" | gpg --import
