92d706c79335c286553033eab9f374ff46abfe68
Lars Peter Thomsen <larsp@magenta-aps.dk>
2023-03-24 14:11:18 +0100

    [#55131] justeringer

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -393,6 +393,9 @@ for municipality in MUNICIPALITIES:
     municipality["code"] = int(municipality["code"])
     municipality["tax_percent"] = Decimal(municipality["tax_percent"])
 
+TAX_FORM_U1 = os.environ.get(
+    "TAX_FORM_U1", "http://etaxgps1/eTaxWebCitz1/Suliffinnut/Login.aspx"
+)
 
 # https://docs.djangoproject.com/en/4.1/topics/email/
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
