2dda9d27ebe20e88d4769719de2c8ab40c236a4c
Lars Peter Thomsen <larsp@magenta.dk>
2023-05-23 10:09:50 +0200

    [#56055] File input

---
+++ backend/aka/static/js/fileform.js
@@ -0,0 +1,82 @@
+$(function(){
+    // If all file rows are set (has a value), create a new empty row
+    const updateRows = function(animate) {
+        const policyform = $(this);
+        const container = policyform.find(".file_container");
+        const rows = container.find(".doc_file").not(".prototype");
+        let allFilled = true;
+        if (animate !== false) {
+            animate = true;
+        }
+        rows.each(function() {
+            const $this = $(this);
+            const fileField = $this.find("input[type=file]");
+            const filled = (fileField.val() !== undefined && fileField.val() !== '');
+            $this.find("button").toggle(filled);
+            if (!filled) {
+                allFilled = false;
+            }
+        });
+        if (allFilled) {
+            const clone = container.find(".doc_file.prototype").first().clone();
+            console.log("clone",clone);
+            clone.removeClass("prototype");
+            clone.find("input").val('');
+            const m = /id_form-(\d+)-file_data/.exec(clone.find("input[type=file]").attr("id"));
+            const form_index = m ? m[1] : policyform.index();
+            const row_index = firstIdleIndex(container);
+            const file_name = "form-"+form_index+"-file_data_"+row_index;
+            clone.find("input[type=file]").attr({"id": "id_"+file_name, "name": file_name});
+            const text_name = "form-"+form_index+"-file_description_"+row_index;
+            clone.find("input[type=text]").attr({"id": "id_"+text_name, "name": text_name});
+            clone.change(updateRows.bind(policyform));
+            clone.find(".close").click(removeRow).hide();
+            container.append(clone);
+            console.log("container",container);
+            if (animate) {
+                clone.slideDown();
+            } else {
+                clone.show();
+            }
+        }
+    }
+
+    const getRowIndexes = function(container) {
+        const indexes = {};
+        container.find(".doc_file").not(".prototype").each(function(){
+            const id = $(this).find("input[type=file]").attr("id");
+            const s = id.lastIndexOf("_");
+            if (s !== -1) {
+                indexes[id.substring(s+1)] = $(this);
+            }
+        });
+        return indexes;
+    };
+
+    const firstIdleIndex = function(container) {
+        const indexes = getRowIndexes(container);
+        let i=0;
+        while (indexes[""+i]) {
+            i++;
+        }
+        return i;
+    }
+
+    const removeRow = function() {
+        const row = $(this).parent(".doc_file");
+        row.slideUp(400, function(){
+            row.remove();
+        });
+    }
+
+    $("form").each(function() {
+        $(this).find(".doc_file").change(updateRows.bind(this));
+        $(this).find(".doc_file .close").click(removeRow);
+        updateRows.call(this, false);
+    });
+
+    $("form").submit(function() {
+        $('.doc_file.prototype').remove();
+    });
+
+});
--- backend/obligatorisk_pension/forms.py
+++ backend/obligatorisk_pension/forms.py
@@ -1,10 +1,62 @@
+import re
 from django.conf import settings
 from django import forms
 from django.forms import widgets
 from django.utils.translation import gettext_lazy as _
 
 
-class ObligatoriskPensionForm(forms.Form):
+class FileSetMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for i in range(0, 10):
+            self.fields[f"file_data_{i}"] = forms.FileField(
+                allow_empty_file=True, required=False
+            )
+            self.fields[f"file_description_{i}"] = forms.CharField(
+                max_length=255,
+                widget=widgets.TextInput(
+                    attrs={
+                        "placeholder": _("obligatorisk_pension.filbeskrivelse"),
+                        "data-trans": "obligatorisk_pension.filbeskrivelse",
+                        "data-trans-attr": "placeholder",
+                    }
+                ),
+                required=False,
+            )
+
+    def get_filled_files(self):
+        # Returns a list of tuples (file, description)
+        if self.is_bound:
+            r = re.compile(self.prefix + r"-file_data_(\d+)")
+            files = []
+            for name, file in self.files.items():
+                m = r.match(name)
+                if m:
+                    description = self.cleaned_data.get(
+                        f"file_description_{m.group(1)}", ""
+                    )
+                    files.append(
+                        (
+                            file,
+                            description,
+                        )
+                    )
+            return files
+        return None
+
+    def get_nonfile_data(self):
+        if self.is_bound:
+            return {
+                k: v for k, v in self.cleaned_data.items() if not k.startswith("file_")
+            }
+
+    @property
+    def filefields(self):
+        return (field for field in self if field.name.startswith("file_"))
+
+
+class ObligatoriskPensionForm(FileSetMixin, forms.Form):
     navn = forms.CharField(
         label=_("Navn"),
         required=True,
