8a7b7a07e3e5aae111a803a08e22d8dc8794dacf
Lars Peter Thomsen <larsp@magenta.dk>
2024-03-15 13:03:52 +0100

    [#59520] Mere layout

--- backend/nedskrivning/forms.py
+++ backend/nedskrivning/forms.py
@@ -3,6 +3,8 @@ from aka.forms import CsvUploadMixin
 from django import forms
 from django.core.validators import FileExtensionValidator
 
+from aka.forms import FileField
+
 logger = logging.getLogger(__name__)
 
 
@@ -29,7 +31,7 @@ class NedskrivningForm(forms.Form):
 
 
 class NedskrivningUploadForm(CsvUploadMixin, forms.Form):
-    file = forms.FileField(
+    file = FileField(
         required=True,
         validators=[
             FileExtensionValidator(["csv", "txt"], code="error.invalid_extension")
