89288a30ab22c11bdecacd382bd153df45dd1341
Lars Peter Thomsen <larsp@magenta.dk>
2024-04-12 13:33:47 +0200

    feat: [#59520] Upgrade dependencies (incl python)

--- akap/aka/tests/test_prismeexception.py
+++ akap/aka/tests/test_prismeexception.py
@@ -9,11 +9,11 @@ class BasicTestCase(SimpleTestCase):
             "Der findes ikke en inkassosag med det eksterne ref.nr. 1234",
             "fordring",
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(
+        self.assertEqual(error.code, 250)
+        self.assertEqual(
             error.text, "Der findes ikke en inkassosag med det eksterne ref.nr. 1234"
         )
-        self.assertEquals(
+        self.assertEqual(
             error.message, "Der findes ikke en inkassosag med det eksterne ref.nr. 1234"
         )
         self.assertDictEqual(
@@ -30,11 +30,11 @@ class BasicTestCase(SimpleTestCase):
             "Der findes ikke en inkassosag med det eksterne ref.nr. 1234",
             "fordring",
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(
+        self.assertEqual(error.code, 250)
+        self.assertEqual(
             error.text, "Der findes ikke en inkassosag med det eksterne ref.nr. 1234"
         )
-        self.assertEquals(
+        self.assertEqual(
             error.message, "Der findes ikke en inkassosag med det eksterne ref.nr. 1234"
         )
         self.assertDictEqual(
@@ -50,12 +50,12 @@ class BasicTestCase(SimpleTestCase):
             "Det fremsendte beløb 2.000,00 er større end restsaldoen på inkassosagen 100,00",
             "nedskrivning",
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(
+        self.assertEqual(error.code, 250)
+        self.assertEqual(
             error.text,
             "Det fremsendte beløb 2.000,00 er større end restsaldoen på inkassosagen 100,00",
         )
-        self.assertEquals(
+        self.assertEqual(
             error.message,
             "Det fremsendte beløb 2.000,00 er større end restsaldoen på inkassosagen 100,00",
         )
@@ -74,12 +74,12 @@ class BasicTestCase(SimpleTestCase):
             "Der findes ingen renter for dette CPR/CVR 1234567890 eller for den angivne periode 05-12-2014 06-08-2018",
             "rentenota",
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(
+        self.assertEqual(error.code, 250)
+        self.assertEqual(
             error.text,
             "Der findes ingen renter for dette CPR/CVR 1234567890 eller for den angivne periode 05-12-2014 06-08-2018",
         )
-        self.assertEquals(
+        self.assertEqual(
             error.message,
             "Der findes ingen renter for dette CPR/CVR 1234567890 eller for den angivne periode 05-12-2014 06-08-2018",
         )
@@ -95,9 +95,9 @@ class BasicTestCase(SimpleTestCase):
 
     def testLoentraek(self):
         error = PrismeException(250, "Aftalenummer 1234-AB findes ikke", "loentraek")
-        self.assertEquals(error.code, 250)
-        self.assertEquals(error.text, "Aftalenummer 1234-AB findes ikke")
-        self.assertEquals(error.message, "Aftalenummer 1234-AB findes ikke")
+        self.assertEqual(error.code, 250)
+        self.assertEqual(error.text, "Aftalenummer 1234-AB findes ikke")
+        self.assertEqual(error.message, "Aftalenummer 1234-AB findes ikke")
         self.assertDictEqual(
             error.params, {"nr": "1234-AB", "text": "Aftalenummer 1234-AB findes ikke"}
         )
@@ -107,12 +107,12 @@ class BasicTestCase(SimpleTestCase):
             "Det samme CPR-Nummer 1234567890 må kun optræde en gang pr. indberetning",
             "loentraek",
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(
+        self.assertEqual(error.code, 250)
+        self.assertEqual(
             error.text,
             "Det samme CPR-Nummer 1234567890 må kun optræde en gang pr. indberetning",
         )
-        self.assertEquals(
+        self.assertEqual(
             error.message,
             "Det samme CPR-Nummer 1234567890 må kun optræde en gang pr. indberetning",
         )
@@ -128,9 +128,9 @@ class BasicTestCase(SimpleTestCase):
         error = PrismeException(
             250, "Der findes ingen debitorer for dette CPR/CVR", "loentraek"
         )
-        self.assertEquals(error.code, 250)
-        self.assertEquals(error.text, "Der findes ingen debitorer for dette CPR/CVR")
-        self.assertEquals(error.message, "Der findes ingen debitorer for dette CPR/CVR")
+        self.assertEqual(error.code, 250)
+        self.assertEqual(error.text, "Der findes ingen debitorer for dette CPR/CVR")
+        self.assertEqual(error.message, "Der findes ingen debitorer for dette CPR/CVR")
         self.assertDictEqual(
             error.params, {"text": "Der findes ingen debitorer for dette CPR/CVR"}
         )
